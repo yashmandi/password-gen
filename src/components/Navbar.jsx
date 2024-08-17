22c4e1cac487b433af5704ad287b6482073176ef
yashmandi <yashmandi18@gmail.com>
2024-08-17 10:23:19 +0530

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="flex justify-between bg-indigo-600 p-2 pl-4 rounded-xl">
+        <h1 className="text-3xl font-extrabold text-white ">PasswordGenerator🔐</h1>
+        <h1> </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
