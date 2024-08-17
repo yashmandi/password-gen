f6b47d3ec08750b1532d5e2e365ff9eb82484e50
yashmandi <yashmandi18@gmail.com>
2024-08-17 10:28:06 +0530

    navbar ui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,17 @@ const Navbar = () => {
   return (
     <div>
       <div className="flex justify-between bg-indigo-600 p-2 pl-4 rounded-xl">
-        <h1 className="text-3xl font-extrabold text-white ">PasswordGenerator🔐</h1>
-        <h1> </h1>
+        <div className="text-3xl font-extrabold text-white ">
+          PasswordGenerator🔐
+        </div>
+        <div className="flex gap-6 pr-2 mt-1">
+          <div className="hover:text-white text-gray-300 cursor-pointer transition">
+            About
+          </div>
+          <div className="hover:text-white text-gray-300 cursor-pointer transition">
+            GitHub
+          </div>
+        </div>
       </div>
     </div>
   );
