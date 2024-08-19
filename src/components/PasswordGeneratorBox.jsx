87be1262f5cf2acfb33d43948b87a11192ce8b41
yashmandi <yashmandi18@gmail.com>
2024-08-19 16:16:06 +0530

    ui

--- src/components/PasswordGeneratorBox.jsx
+++ src/components/PasswordGeneratorBox.jsx
@@ -14,7 +14,7 @@ const PasswordGeneratorBox = () => {
       newPassword += characters[randomIndex];
     }
     setPassword(newPassword);
-    setCopied(false); 
+    setCopied(false);
   };
 
   const copyToClipboard = () => {
@@ -26,7 +26,7 @@ const PasswordGeneratorBox = () => {
     <div className="flex items-center justify-center mt-24 px-4">
       <div className="w-full max-w-md p-8  border-gray-800 shadow-lg flex flex-col items-center rounded-lg bg-gradient-to-b from-[#202d3a] to-[#1c2129]">
         <button
-          className="btn bg-gradient-to-r from-indigo-500 to-purple-600 hover:from-indigo-600 hover:to-purple-700 py-3 px-6 rounded-lg text-white text-lg font-semibold tracking-wide shadow-lg transform transition-transform duration-300 hover:scale-105"
+          className="btn bg-gradient-to-r from-indigo-500 to-purple-600 hover:from-indigo-600 hover:to-purple-700 py- px- rounded-lg text-white text-lg font-semibold tracking-wide shadow-lg transform transition-transform duration-300 hover:scale-[1.02] hover:shadow-2xl"
           onClick={generatePassword}
         >
           Generate a Strong Password
@@ -40,15 +40,15 @@ const PasswordGeneratorBox = () => {
               className="p-3 rounded-lg bg-gray-800 text-white w-full mr-2 text-center font-mono text-lg tracking-wider shadow-inner"
             />
             <button
-              className={`btn py-3 px-4 rounded-lg text-white font-semibold shadow-md transform transition-transform duration-300 ${
-                copied
-                  ? "bg-green-500 hover:bg-green-600 scale-105"
-                  : "bg-indigo-500 hover:bg-indigo-600"
-              }`}
+              className={`btn py-3 px-4 rounded-lg font-semibold shadow-md transform transition-all duration-300 ${copied
+                ? "bg-gradient-to-r from-green-500 to-blue-600 hover:from-purple-600 hover:to-blue-700 text-white scale-100 hover:scale-[1.02]"
+                : "bg-gradient-to-r from-blue-500 to-purple-600 hover:from-blue-600 hover:to-purple-700 text-white scale-100 hover:scale-[1.02]"
+                }`}
               onClick={copyToClipboard}
             >
               {copied ? "Copied!" : "Copy"}
             </button>
+
           </div>
         )}
       </div>
