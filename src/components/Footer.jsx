a1f48d1f0b28bc141928311d9f93d831820df0d3
yashmandi <yashmandi18@gmail.com>
2024-08-19 16:37:15 +0530

    s

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="fixed bottom-0 left-0 w-full  text-center py-4">
+      <p className='text-gray-400 text-sm'>
+        Developed by{' '}
+        <a
+          href="https://github.com/yashmandi"
+          target="_blank"
+          rel="noopener noreferrer"
+          className=" hover:underline text-gray-200"
+        >
+          @yashmandi
+        </a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import PasswordGeneratorBox from '../components/PasswordGeneratorBox'
+import React from 'react';
+import Navbar from '../components/Navbar';
+import PasswordGeneratorBox from '../components/PasswordGeneratorBox';
+import Footer from '../components/Footer';
 
 const Home = () => {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen pb-16"> {/* Adjust padding-bottom to avoid overlap with the footer */}
       <Navbar />
-      <PasswordGeneratorBox />
+      <main className="flex-grow">
+        <PasswordGeneratorBox />
+      </main>
+      <Footer/>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
