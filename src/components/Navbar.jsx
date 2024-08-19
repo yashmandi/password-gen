96ebc7c8f1c0575c4fe50b14947ee7171f3eb0b5
yashmandi <yashmandi18@gmail.com>
2024-08-19 16:49:30 +0530

    s

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div>
       <div className="flex justify-between bg-indigo-600 p-3 pl-4 rounded-xl">
-        <div className="text-3xl font-extrabold text-white ">
+        <div className="text-2xl sm:text-3xl font-extrabold text-white">
           PasswordGenerator🔐
         </div>
-        <div className="flex gap-6 pr-2 mt-1">
-            <div className="hover:text-white text-gray-300 cursor-pointer transition">
-              About
-            </div>
-          <a target="_blank" href="https://github.com/yashmandi/password-gen">
-            <div className="hover:text-white text-gray-300 cursor-pointer transition">
+        <div className="flex gap-4 sm:gap-6 pr-2 mt-1">
+          <a
+            target="_blank"
+            href="https://github.com/yashmandi/password-gen"
+            className="text-sm sm:text-base"
+          >
+            <div className="hover:text-white  text-gray-300 cursor-pointer transition">
               GitHub
             </div>
           </a>
