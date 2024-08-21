3ead4770b2e0ebc74df43041348af8417f13f54f
yashmandi <yashmandi18@gmail.com>
2024-08-21 18:31:44 +0530

    hero ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,8 +10,8 @@ const Home = () => {
       <Navbar />
       <div>
         <p className="text-3xl pt-16 font-bold text-center mt-10 text-white">
-          Effortlessly create a strong, random<br />
-          password with <span className="text-[#4F46E5] hover:text-indigo-400 font-extrabold  transition cursor-default">PassGen</span>🔐
+          Instantly generate a strong, random<br />
+          password with <span className="text-[#766cf6] hover:text-indigo-400 font-extrabold  transition cursor-default">PassGen</span>🔐
         </p>
         <p className="text-md text-center mt-4 text-gray-400">
           Designed to keep your accounts <span className="text-gray-300 font-semibold">safe</span> and <span className="text-gray-300 font-semibold">secure</span>
