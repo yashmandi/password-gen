ef5db2d3e44000e0f38bc5aa8bd09871e28f30d1
yashmandi <yashmandi18@gmail.com>
2024-08-21 18:48:37 +0530

    section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,18 +11,26 @@ const Home = () => {
       <Navbar />
       <div>
         <p className="text-3xl pt-16 font-bold text-center mt-10 text-white">
-          Instantly generate a strong, random<br />
-          password with <span className="text-[#766cf6] hover:text-indigo-400 font-extrabold  transition cursor-default">PassGen</span>🔐
+          Instantly generate a strong, random
+          <br />
+          password with{" "}
+          <span className="text-[#766cf6] hover:text-indigo-400 font-extrabold  transition cursor-default">
+            PassGen
+          </span>
+          🔐
         </p>
         <p className="text-md text-center mt-4 text-gray-400">
-          Designed to keep your accounts <span className="text-gray-300 font-semibold">safe</span> and <span className="text-gray-300 font-semibold">secure</span>
+          Designed to keep your accounts{" "}
+          <span className="text-gray-300 font-semibold">safe</span> and{" "}
+          <span className="text-gray-300 font-semibold">secure</span>
         </p>
       </div>
       <main className="flex-grow">
         <PasswordGeneratorBox />
-      <hr className="border-gray-700 mt-24"/>
+        <hr className="border-gray-700 mt-24" />
       </main>
       <Section />
+      <hr className="border-gray-700 mt-20 mb-24" />
       <Footer />
     </div>
   );
