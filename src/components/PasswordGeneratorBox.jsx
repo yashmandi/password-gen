3490f4ca96b8235e870e6e8a5cbe7c896ddd19ad
yashmandi <yashmandi18@gmail.com>
2024-08-21 17:48:40 +0530

    slider

--- src/components/PasswordGeneratorBox.jsx
+++ src/components/PasswordGeneratorBox.jsx
@@ -24,7 +24,7 @@ const PasswordGeneratorBox = () => {
 
   return (
     <div className="flex items-center justify-center mt-24 px-4">
-      <div className="w-full max-w-md p-8 border-gray-800 shadow-lg flex flex-col items-center rounded-lg bg-gradient-to-b from-[#243242] to-[#1c2129]">
+      <div className="w-3/5 p-8 border-gray-800 shadow-lg flex flex-col items-center rounded-lg bg-gradient-to-b from-[#243242] to-[#1c2129]">
         <label className="text-white mb-4 text-lg font-semibold">
           Password Length: {passwordLength}
         </label>
@@ -39,7 +39,7 @@ const PasswordGeneratorBox = () => {
         />
 
         <button
-          className="btn bg-gradient-to-r from-indigo-500 to-purple-600 hover:from-indigo-600 hover:to-purple-700 px-4 rounded-lg text-white text-base sm:text-lg w-full font-semibold tracking-wide shadow-lg transform transition-transform duration-300 hover:scale-[1.02] hover:shadow-2xl"
+          className="btn bg-gradient-to-r from-indigo-500 to-purple-600 hover:from-indigo-600 hover:to-purple-700 px-4 rounded-lg text-white text-base sm:text-lg w-80 font-semibold tracking-wide shadow-lg transform transition-transform duration-300 hover:scale-[1.02] hover:shadow-2xl"
           onClick={generatePassword}
         >
           Generate a Strong Password!
@@ -47,12 +47,14 @@ const PasswordGeneratorBox = () => {
 
         {password && (
           <div className="flex flex-col sm:flex-row justify-between items-center mt-8 w-full">
+            {/* <div> </div> */}
             <input
               type="text"
               value={password}
               readOnly
-              className="p-2 rounded-lg bg-gray-700 text-white w-full sm:w-72 mb-4 sm:mb-0 sm:mr-2 text-center font-mono text-lg tracking-wider shadow-inner"
+              className="p-2 rounded-lg bg-gray-700 text-white w-full sm:w-full mr-4 mb-4 sm:mb-0 sm:mr-2 text-center font-mono text-lg tracking-wider shadow-inner"
             />
+
             <button
               className={`btn py-3 px-4 rounded-lg font-semibold shadow-md transform transition-all duration-300 ${
                 copied
