f573f32c6f160656feb78b038a00c6ed69f9cc35
yashmandi <yashmandi18@gmail.com>
2024-08-22 20:28:36 +0530

    div center in section

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -5,7 +5,7 @@ const Section = () => {
     <div>
       <section className="">
         <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
-          <div className="max-w-screen-md mb-8 lg:mb-16">
+          <div className="max-w-screen-md mb-8 lg:mb-16 mx-auto text-center">
             <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-white">Designed for business teams like yours</h2>
             <p className="text-gray-500 sm:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
           </div>
