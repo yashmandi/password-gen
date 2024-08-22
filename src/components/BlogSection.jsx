df83242190564cac7007ccadd1470174a04e27e0
yashmandi <yashmandi18@gmail.com>
2024-08-22 21:31:12 +0530

    blog page

--- src/components/BlogSection.jsx
+++ src/components/BlogSection.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const BlogSection = () => {
   return (
@@ -9,30 +10,32 @@ const BlogSection = () => {
             Read about password best practices!
           </h1>
           <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
-            Explore our comprehensive blog for expert advice on creating
-            strong, secure passwords. Learn best practices, stay updated on the
-            latest security trends, and ensure your online safety with our
-            in-depth articles.
+            Explore our comprehensive blog for expert advice on creating strong,
+            secure passwords. Learn best practices, stay updated on the latest
+            security trends, and ensure your online safety with our in-depth
+            articles.
           </p>
           <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-            <a
-              href="#"
-              className="btn bg-gradient-to-r from-indigo-500 to-purple-600 hover:from-indigo-600 hover:to-purple-700 px-4 rounded-lg text-white text-base sm:text-md w-80 font-semibold tracking-wide shadow-lg transform transition-transform duration-300  hover:shadow-2xl w-44 mt-2"
-            >
-              Learn more
-              <svg
-                className="ml-2 -mr-1 w-5 h-5"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
+            <Link to="/blogs">
+              <a
+                href="#"
+                className="btn bg-gradient-to-r from-indigo-500 to-purple-600 hover:from-indigo-600 hover:to-purple-700 px-4 rounded-lg text-white text-base sm:text-md w-80 font-semibold tracking-wide shadow-lg transform transition-transform duration-300  hover:shadow-2xl w-44 mt-2"
               >
-                <path
-                  fill-rule="evenodd"
-                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-            </a>
+                Learn more
+                <svg
+                  className="ml-2 -mr-1 w-5 h-5"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </a>
+            </Link>
           </div>
         </div>
       </section>
