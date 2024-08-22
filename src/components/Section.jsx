1459962cae5a2441f0b1dcbf0c3a1950595ee365
yashmandi <yashmandi18@gmail.com>
2024-08-22 20:26:51 +0530

    section ui updatre

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -10,17 +10,20 @@ const Section = () => {
             <p className="text-gray-500 sm:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
           </div>
           <div className="space-y-8  md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
-            <div className="bg-gradient-to-r from-indigo-500 to-purple-600 text-white p-10 rounded-xl shadow-gray-900 hover:bg-gradient-to-b from-purple-600 to-indigo-600 shadow-xl ">
-              <h3 className="mb-2 text-xl font-bold dark:text-white">Marketing</h3>
-              <p className="text-white">Plan it, create it, launch it. Collaborate seamlessly with all  the organization and hit your marketing goals every month with our marketing plan.</p>
+            <div className="bg-gradient-to-r from-indigo-500 to-purple-600 text-white p-10 rounded-xl shadow-gray-900 hover:bg-gradient-to-b from-purple-700 to-indigo-500 shadow-xl ">
+              <h3 className="mb-2 text-xl font-bold dark:text-white">Customizable</h3>
+              <p className="text-white">Easily adjust the length of your passwords to meet specific security requirements or personal preferences.
+              </p>
             </div>
-            <div className="bg-gradient-to-r from-indigo-500 to-purple-600 text-white p-10 rounded-xl shadow-gray-900 hover:bg-gradient-to-b from-purple-600 to-indigo-600 shadow-xl ">
-              <h3 className="mb-2 text-xl font-bold dark:text-white">Marketing</h3>
-              <p className="text-white">Plan it, create it, launch it. Collaborate seamlessly with all  the organization and hit your marketing goals every month with our marketing plan.</p>
+            <div className="bg-gradient-to-r from-indigo-500 to-purple-600 text-white p-10 rounded-xl shadow-gray-900 hover:bg-gradient-to-b from-purple-700 to-indigo-500 shadow-xl ">
+              <h3 className="mb-2 text-xl font-bold dark:text-white">Instant </h3>
+              <p className="text-white">Generate strong, random passwords instantly to ensure your accounts remain secure without any hassle.
+              </p>
             </div>
-            <div className="bg-gradient-to-r from-indigo-500 to-purple-600 text-white p-10 rounded-xl shadow-gray-900 hover:bg-gradient-to-b from-purple-600 to-indigo-600 shadow-xl ">
-              <h3 className="mb-2 text-xl font-bold dark:text-white">Marketing</h3>
-              <p className="text-white">Plan it, create it, launch it. Collaborate seamlessly with all  the organization and hit your marketing goals every month with our marketing plan.</p>
+            <div className="bg-gradient-to-r from-indigo-500 to-purple-600 text-white p-10 rounded-xl shadow-gray-900 hover:bg-gradient-to-b from-purple-700 to-indigo-500 shadow-xl ">
+              <h3 className="mb-2 text-xl font-bold dark:text-white">Secure</h3>
+              <p className="text-white">Ensure your passwords are strong and random to protect your accounts effectively.
+              </p>
             </div>
           </div>
         </div>
