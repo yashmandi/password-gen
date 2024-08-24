fb4ad48de0d0eb236393ef4a290e8f4746095361
yashmandi <yashmandi18@gmail.com>
2024-08-24 16:41:04 +0530

    toasted

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Toaster } from 'react-hot-toast';
 import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Toaster />
   </StrictMode>,
 )
