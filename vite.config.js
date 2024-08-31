f9f31cce4651218ee6d04f38af15a653f28b86b2
yashmandi <yashmandi18@gmail.com>
2024-08-31 22:02:40 +0530

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [react()],
+    define: {
+      'process.env': env
+    }
+  }
+}
