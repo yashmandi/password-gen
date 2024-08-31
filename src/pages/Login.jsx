89d25faa8da932768762e01d5d9c86085f25e511
yashmandi <yashmandi18@gmail.com>
2024-08-31 22:12:28 +0530

    updated backend depoyed url

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch(`${baseURL}/login`, {
+      const response = await fetch(`${baseURL}login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
