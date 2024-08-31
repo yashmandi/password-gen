d43121dc25c24dcc29c5631e7f69b045421d1611
yashmandi <yashmandi18@gmail.com>
2024-08-31 10:15:25 +0530

    signup page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -146,23 +146,6 @@ const Signup = () => {
                 </div>
                 <div className="flex items-center justify-between">
                   <div className="flex items-start">
-                    <div className="flex items-center h-5">
-                      <input
-                        id="remember"
-                        aria-describedby="remember"
-                        type="checkbox"
-                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                        required
-                      />
-                    </div>
-                    <div className="ml-3 text-sm">
-                      <label
-                        htmlFor="remember"
-                        className="text-gray-500 dark:text-gray-300"
-                      >
-                        Remember me
-                      </label>
-                    </div>
                   </div>
                 </div>
                 <button
